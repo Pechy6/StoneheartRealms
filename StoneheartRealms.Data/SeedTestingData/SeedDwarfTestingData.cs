eaed296094907d3aadf51aaec6fed872d6edff2d
Pechy6 <Pechyy6@gmail.com>
2026-06-01 20:31:18 +0200

    feat: add seed data into classes

--- StoneheartRealms.Data/SeedTestingData/SeedDwarfTestingData.cs
+++ StoneheartRealms.Data/SeedTestingData/SeedDwarfTestingData.cs
@@ -1,6 +1,55 @@
-namespace StoneheartRealms.Data.TestingData;
+using Microsoft.EntityFrameworkCore;
+using StoneheartRealms.Data.Entities.Creatures;
 
-public class DwarfTestingData
+namespace StoneheartRealms.Data.SeedTestingData;
+
+public static class SeedDwarfTestingData
 {
-    
+    public static void SeedDwarf(ModelBuilder modelBuilder)
+    {
+        modelBuilder.
+            Entity<Dwarf>().
+            HasData(
+                new Dwarf
+                {
+                    Id = 1,
+                    Name = "First Dwarf",
+                    Description = "This is the first dwarf",
+                    Age = 186,
+                    Gender = Gender.Male
+                },
+                new Dwarf
+                {
+                    Id = 2,
+                    Name = "Second Dwarf",
+                    Description = "This is the second dwarf",
+                    Age = 186,
+                    Gender = Gender.Female
+                },
+                new Dwarf
+                {
+                    Id = 3,
+                    Name = "Third Dwarf",
+                    Description = "This is the third dwarf",
+                    Age = 186,
+                    Gender = Gender.Male
+                },
+                new Dwarf
+                {
+                    Id = 4,
+                    Name = "Fourth Dwarf",
+                    Description = "This is the fourth dwarf",
+                    Age = 186,
+                    Gender = Gender.Male
+                },
+                new Dwarf
+                {
+                    Id = 5,
+                    Name = "Fifth Dwarf",
+                    Description = "This is the fifth dwarf",
+                    Age = 186,
+                    Gender = Gender.Female
+                }
+            );
+    }
 }
--- StoneheartRealms.Data/SeedTestingData/SeedJobTestingData.cs
+++ StoneheartRealms.Data/SeedTestingData/SeedJobTestingData.cs
@@ -1,6 +1,33 @@
-namespace StoneheartRealms.Data.TestingData;
+using Microsoft.EntityFrameworkCore;
+using StoneheartRealms.Data.Entities.Jobs;
 
-public class JobTestingData
+namespace StoneheartRealms.Data.SeedTestingData;
+
+public static class SeedJobTestingData
 {
-    
+    public static void SeedJob(ModelBuilder modelBuilder)
+    {
+        modelBuilder.
+            Entity<Job>().
+            HasData(
+                new Job
+                {
+                    Id = 1,
+                    Name = "Farmer",
+                    Description = "This is a farmer"
+                },
+                new Job
+                {
+                    Id = 2,
+                    Name = "Miner",
+                    Description = "This is a miner"
+                },
+                new Job
+                {
+                    Id = 3,
+                    Name = "Cook",
+                    Description = "This is a cook"
+                }
+            );
+    }
 }
