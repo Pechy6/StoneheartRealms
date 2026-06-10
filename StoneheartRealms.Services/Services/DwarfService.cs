0956aee97538e1b99f84029419701422dd4e809a
Pechy6 <Pechyy6@gmail.com>
2026-06-10 13:35:17 +0200

    feat: Add GetDwarf into DwarfService

--- StoneheartRealms.Services/Services/DwarfService.cs
+++ StoneheartRealms.Services/Services/DwarfService.cs
@@ -1,6 +1,30 @@
+using StoneheartRealms.Data.Data;
+using StoneheartRealms.Services.DTOs;
+using StoneheartRealms.Services.Interfaces;
+
 namespace StoneheartRealms.Services.Services;
 
-public class DwarfService
+public class DwarfService(StoneheartRealmsDbContext context) : IDwarfService
 {
-    
+    private readonly StoneheartRealmsDbContext _context = context;
+
+    public async Task<DwarfDto?> GetDwarf(int id)
+    {
+        var dwarf = await _context.Dwarves.FindAsync(id);
+        if (dwarf == null)
+            return null;
+        
+        var dwarfDto = new DwarfDto
+        {
+            Id = dwarf.Id,
+            Name = dwarf.Name,
+            Description = dwarf.Description,
+            Age = dwarf.Age,
+            Gender = dwarf.Gender,
+            Energy = dwarf.Energy,
+            Hunger = dwarf.Hunger,
+            Thirst = dwarf.Thirst
+        };
+        return dwarfDto;
+    }
 }
