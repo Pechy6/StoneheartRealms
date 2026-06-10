76d7c8f391ecd7dae6efc1c3deaa2f749e72a980
Pechy6 <Pechyy6@gmail.com>
2026-06-10 13:34:37 +0200

    feat: add properties into Dtos

--- StoneheartRealms.Services/DTOs/CreateDwarfDto.cs
+++ StoneheartRealms.Services/DTOs/CreateDwarfDto.cs
@@ -1,6 +1,11 @@
+using StoneheartRealms.Data.Entities.Creatures;
+
 namespace StoneheartRealms.Services.DTOs;
 
 public class CreateDwarfDto
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Age { get; set; }
+    public Gender Gender { get; set; }
 }
--- StoneheartRealms.Services/DTOs/UpdateDwarfDto.cs
+++ StoneheartRealms.Services/DTOs/UpdateDwarfDto.cs
@@ -1,6 +1,10 @@
+using StoneheartRealms.Data.Entities.Creatures;
+
 namespace StoneheartRealms.Services.DTOs;
 
 public class UpdateDwarfDto
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Gender Gender { get; set; }
 }
