aaf91a06ae4fe473d1d5d7cb9c5a9068dba7d62a
Pechy6 <Pechyy6@gmail.com>
2026-06-10 12:59:12 +0200

    feat: Add properities into DwarfDto

--- StoneheartRealms.Services/DTOs/DwarfDto.cs
+++ StoneheartRealms.Services/DTOs/DwarfDto.cs
@@ -1,6 +1,17 @@
+using StoneheartRealms.Data.Entities.Creatures;
+
 namespace StoneheartRealms.Services.DTOs;
 
 public class DwarfDto
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Description { get; set; } = "";
+    public int Age { get; set; }
     
+    public Gender Gender { get; set; }
+    
+    public int Energy { get; set; }
+    public int Hunger { get; set; }
+    public int Thirst { get; set; }
 }
