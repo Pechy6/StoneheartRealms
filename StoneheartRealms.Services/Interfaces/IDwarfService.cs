6a1e08ffc701187c52748d7d094956b00f5c7ac8
Pechy6 <Pechyy6@gmail.com>
2026-06-10 13:05:17 +0200

    feat: Add Crud operation into Interface

--- StoneheartRealms.Services/Interfaces/IDwarfService.cs
+++ StoneheartRealms.Services/Interfaces/IDwarfService.cs
@@ -1,6 +1,12 @@
+using StoneheartRealms.Services.DTOs;
+
 namespace StoneheartRealms.Services.Interfaces;
 
 public interface IDwarfService
 {
-     
+    public Task<DwarfDto> GetDwarf(int id);
+    public Task<IEnumerable<DwarfDto>> GetDwarves();
+    public Task<DwarfDto> CreateDwarf(DwarfDto dwarf);
+    public Task<DwarfDto> UpdateDwarf(DwarfDto dwarf);
+    public Task DeleteDwarf(int id);
 }
