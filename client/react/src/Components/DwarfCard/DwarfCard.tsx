4adf299816808baabc856524ec920ea0fe395898
Pechy6 <Pechyy6@gmail.com>
2026-06-15 14:55:26 +0200

    feat: add delete button into dwarf detail with confirmation

--- client/react/src/Components/DwarfCard/DwarfCard.tsx
+++ client/react/src/Components/DwarfCard/DwarfCard.tsx
@@ -18,15 +18,25 @@ type DwarfDetailProps = {
 
 export const DwarfCard = ({id, isModalOpen, setIsModalOpen}: DwarfDetailProps) => {
     const [dwarf, setDwarf] = useState<Dwarf | null>(null);
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
     const fetchDwarf = async (id: number) => {
         const response = await fetch(`/api/dwarves/${id}`);
         const data = await response.json();
         setDwarf(data);
     }
-    
-    const ender = () => {
-        console.log("ender")
+
+    const fetchDelete = async (id: number) => {
+        const response = await fetch(`/api/dwarves/${id}`, {
+            method: 'DELETE'
+        });
+        if (response.ok) {
+            setIsModalOpen(false);
+        }
+    }
+
+    const handleDelete = () => {
+        setShowDeleteConfirmation(true);
     }
 
     useEffect(() => {
@@ -35,25 +45,36 @@ export const DwarfCard = ({id, isModalOpen, setIsModalOpen}: DwarfDetailProps) =
 
     if (!isModalOpen)
         return null;
-    
+
     if (!dwarf) {
         return <div>Loading...</div>;
     }
-    
+
     return (
-    <div className="DwarfCard">
-        <button onClick={() => setIsModalOpen(false)}>X</button>
-        <h1>Detail</h1>
-        <ul>
-            <li><span>Name:</span> {dwarf.name}</li>
-            <li><span>Age:</span> {dwarf.age}</li>
-            <li><span>Gender:</span> {dwarf.gender}</li>
-            <li><span>Description:</span> {dwarf.description}</li>
-            <li><span>Energy:</span> {dwarf.energy}</li>
-            <li><span>Hunger:</span> {dwarf.hunger}</li>
-            <li><span>Thirst:</span> {dwarf.thirst}</li>
-        </ul>
-        
-    </div>
-)
+        <div className="DwarfCard">
+            <button className="close-button" onClick={() => setIsModalOpen(false)}>X</button>
+            <h1>Detail</h1>
+            <ul>
+                <li><span>Name:</span> {dwarf.name}</li>
+                <li><span>Age:</span> {dwarf.age}</li>
+                <li><span>Gender:</span> {dwarf.gender}</li>
+                <li><span>Description:</span> {dwarf.description}</li>
+                <li><span>Energy:</span> {dwarf.energy}</li>
+                <li><span>Hunger:</span> {dwarf.hunger}</li>
+                <li><span>Thirst:</span> {dwarf.thirst}</li>
+                <button onClick={handleDelete}>Delete</button>
+            </ul>
+            
+            {showDeleteConfirmation && (
+                <div className="delete-confirmation">
+                    <p>Are you sure you want to delete this dwarf {dwarf.name}?</p>
+                    <button onClick={() => fetchDelete(id)}>Yes</button>
+                    
+                    <button onClick={() => setShowDeleteConfirmation(false)}>No</button>
+                </div>
+
+
+            )}
+        </div>
+    )
 }
